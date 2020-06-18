dd38b1f04b93c4252a705ed6fab8c044cc1021bc
muradnasibli <muradin@code.edu.az>
2020-06-18 16:01:50 +0400

    Search by product name added

---
+++ eCommerceApp.Web/ViewComponents/SearchViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using eCommerceApp.DAL.Repositories.Abstact;
+using eCommerceApp.Domains;
+using eCommerceApp.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Web.ViewComponents
+{
+    public class SearchViewComponent : ViewComponent
+    {
+       private IRepository<Category> _repo;
+
+        public SearchViewComponent(IRepository<Category> repo)
+        {
+            _repo = repo;
+        }
+
+        public ViewViewComponentResult Invoke()
+        {
+            CategoryListViewModel model = new CategoryListViewModel
+            {
+                Categories = _repo.GetAll().ToList(),
+                CurrentCategory = Convert.ToInt32(HttpContext.Request.Query["category"])
+            };
+            return View(model);
+        }
+    }
+}
